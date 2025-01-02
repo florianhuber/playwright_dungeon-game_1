548b01a888621664a4828828321176cfe13de136
Florian Huber <florian.huber@semantic-web.com>
2025-01-02 22:06:27 +0100

    dungeon game 1 test based on codegen output

---
+++ tests/dungeon-game_1-codegen.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await expect(page.getByRole("heading")).toContainText(
+    "Welcome to the Dungeon!"
+  );
+  await page.getByRole("link", { name: "Open the right door" }).click();
+  await expect(page.getByRole("heading")).toContainText("The Right Door");
+  await page.getByRole("link", { name: "Return to the entrance" }).click();
+  await page.getByRole("link", { name: "Open the left door" }).click();
+  await expect(
+    page.getByRole("link", { name: "Fight the goblin" })
+  ).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "Run back to the entrance" })
+  ).toBeVisible();
+  await page.getByRole("link", { name: "Fight the goblin" }).click();
+  await expect(page.getByRole("button", { name: "Roll Dice" })).toBeVisible();
+  await page.getByRole("button", { name: "Roll Dice" }).click();
+  await expect(page.getByText("You rolled a 4. You defeated")).toBeVisible();
+  await expect(page.locator("#result")).toContainText("You rolled");
+  await page.getByRole("link", { name: "Return to the entrance" }).click();
+  await page.getByRole("link", { name: "Open the straight door" }).click();
+  await expect(page.getByRole("paragraph")).toContainText(
+    "You find a chest filled with gold and jewels."
+  );
+  await page.getByRole("link", { name: "Return to the entrance" }).click();
+});
